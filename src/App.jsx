959a82e643b5a9b8dab36289700e2ba84dec2876
Sandeep Murmu <sandeepmurmu99@gmail.com>
2025-08-19 19:29:50 +0530

    update responsive style for job item list

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import JobItem from "./components/jobItem";
 
 function App() {
   return (
-    <div className="job-list w-full flex flex-col items-center gap-7">
+    <div className="job-list w-full flex flex-col items-center sm:gap-7 gap-18 px-6 pt-15 sm:pt-0">
       {data.map((job) => (
         <JobItem key={job.id} jobObj={job} />
       ))}
--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -4,15 +4,15 @@ function JobItem({ jobObj }) {
 
   return (
     <div
-      className={`job-item rounded-md shadow-[0_10px_15px_-3px_rgba(91,164,164,0.3)]  flex items-center p-6.5 w-full max-w-5xl justify-between bg-white  ${
-        jobObj.featured && "border-l-4 border-l-(--Green-400)"
+      className={`job-item rounded-md shadow-[0_10px_15px_-3px_rgba(91,164,164,0.3)] flex items-center justify-between flex-wrap gap-8 sm:gap-4 p-6.5 w-full max-w-5xl pt-15 sm:pt-6.5 bg-white relative ${
+        jobObj.featured && "border-l-5 border-l-(--Green-400)"
       }`}
     >
-      <div className="job-logo pr-5">
+      <div className="job-logo absolute sm:static top-[0%] translate-y-[-50%] sm:translate-y-0">
         <img src={jobObj.logo} alt={jobObj.company} />
       </div>
-      <div className="job-info mr-auto">
-        <div className="job-info__top flex items-center mb-1">
+      <div className="job-info mr-auto ">
+        <div className="job-info__top flex flex-wrap items-center mb-1">
           <h3 className="company-name font-bold mr-4 text-(--Green-400)">
             {jobObj.company}
           </h3>
@@ -24,17 +24,17 @@ function JobItem({ jobObj }) {
           </div>
         </div>
         <div className="job-info__main">
-          <h2 className="job-position text-xl font-bold hover:text-(--Green-400) cursor-pointer transition-colors mb-1">
+          <h2 className="job-position text-xl font-bold hover:text-(--Green-400) cursor-pointer transition-colors my-2">
             {jobObj.position}
           </h2>
-          <ul className="job-detail flex list-disc text-(--Gray-400) gap-8 indent-2 ml-[-8px]">
+          <ul className="job-detail flex flex-wrap list-disc text-(--Gray-400) gap-8 indent-2 ml-[-8px]">
             <li className="list-none">{jobObj.postedAt}</li>
             <li>{jobObj.contract}</li>
             <li>{jobObj.location}</li>
           </ul>
         </div>
       </div>
-      <div className="job-holdings flex gap-4 text-(--Green-400) font-semibold *:bg-(--Green-50) *:px-2 *:py-1 text-sm *:rounded-sm *:cursor-pointer *:hover:bg-(--Green-400) *:hover:text-(--Green-50) transition-all">
+      <div className="job-holdings w-full sm:w-auto flex flex-wrap gap-4 text-(--Green-400) font-semibold *:bg-(--Green-50) *:px-2 *:py-1 text-sm *:rounded-sm *:cursor-pointer *:hover:bg-(--Green-400) *:hover:text-(--Green-50) transition-all relative before:absolute before:w-full before:h-0.5 before:bg-(--Gray-400) before:top-[-18px] sm:before:hidden">
         <button className="job-role">{jobObj.role}</button>
         <button className="job-level">{jobObj.level}</button>
 
